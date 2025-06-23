fc9e6338f9aa13adbb8290cb456a8c0511623160
cabdiakariim <cabdikariimmaxamaddaahir@gmail.com>
2025-06-23 17:16:59 +0300

    starting nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,vue,js}", // ✅ Scan all HTML, Vue, and JS files in /src
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Montserrat', 'sans-serif']
+      }
+    },
+  
     screens: {
       'xs': { 'max': '639px' }, // 0 - 639px
       'sm': '640px',
