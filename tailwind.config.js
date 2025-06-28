5905b6df5b1c8b8b457636cb0ee57c04f0aacdda
cabdiakariim <cabdikariimmaxamaddaahir@gmail.com>
2025-06-28 16:05:54 +0300

    finishing the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 // tailwind.config.js
-module.exports = {
-  content: [
-    "./src/**/*.{html,vue,js}", // ✅ Scan all HTML, Vue, and JS files in /src
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Montserrat', 'sans-serif']
-      }
-    },
-  
-    screens: {
-      'xs': { 'max': '639px' }, // 0 - 639px
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px'
-    },
+export const content = [
+  "./src/**/*.{html,vue,js}", // ✅ Scan all HTML, Vue, and JS files in /src
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      poppins: ['Montserrat', 'sans-serif']
+    }
   },
-  plugins: [],
+
+  screens: {
+    'xs': { 'max': '639px' }, // 0 - 639px
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px'
+  },
 };
+export const plugins = [];
